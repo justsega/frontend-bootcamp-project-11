1973bf5fd6001350f8528c1c0a107982af346a66
Sergey Syromyatnikov <sergsyrom@gmail.com>
2023-03-29 19:57:33 +0500

    upd

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-import { path } from 'path';
-import { HtmlWebpackPlugin } from 'html-webpack-plugin';
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 import WorkboxWebpackPlugin from 'workbox-webpack-plugin';
 
-const isProduction = process.env.NODE_ENV == 'production';
 
+const isProduction = process.env.NODE_ENV == 'production';
+const __dirname = '/';
 const config = {
   entry: './src/index.js',
   output: {
